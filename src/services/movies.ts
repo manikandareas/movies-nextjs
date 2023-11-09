f82972d35cd9ab3b5df738d707140a410a410c04
manikandareas <vitoandareas15@gmail.com>
2023-11-10 02:07:17 +0800

    service(details movie): adding service details movie

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,9 +1,13 @@
 import { axiosInstances } from "@/common/lib/axios";
 import {
+  CreditsMovie,
+  DetailsMovie,
+  ImagesMovie,
   PopularMovies,
   TMDBResponses,
   TopRatedMovies,
   TrendingMovies,
+  VideosMovie,
 } from "@/types/tmdb";
 
 interface PopularMoviesProps {
@@ -36,4 +40,37 @@ export const getTrendingMovies = async ({
   return response.data as TMDBResponses<TrendingMovies[]>;
 };
 
-export const getMovieDetails = () => {};
+type DetailsMoviesProps = {
+  movie_id?: number;
+};
+export const getMovieDetails = async ({ movie_id }: DetailsMoviesProps) => {
+  const resDetailsMovie = await axiosInstances
+    .get(`/movie/${movie_id}`)
+    .then((res) => res.data as DetailsMovie);
+
+  const resCreditsMovie = await axiosInstances
+    .get(`/movie/${movie_id}/credits`)
+    .then((res) => res.data as CreditsMovie);
+
+  const resImagesMovie = await axiosInstances
+    .get(`/movie/${movie_id}/images`)
+    .then((res) => res.data as ImagesMovie);
+
+  const resVideosMovie = await axiosInstances
+    .get(`/movie/${movie_id}/videos`)
+    .then((res) => res.data as VideosMovie);
+
+  const resSimilarMovie = await axiosInstances
+    .get(`/movie/${movie_id}/similar`)
+    .then((res) => res.data);
+
+  const response = {
+    details: resDetailsMovie,
+    credits: resCreditsMovie,
+    images: resImagesMovie,
+    videos: resVideosMovie,
+    similar: resSimilarMovie,
+  };
+
+  return response;
+};
--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -28,6 +28,127 @@ export interface PopularMovies extends Movies {}
 
 export interface TopRatedMovies extends Movies {}
 
+export interface DetailsMovie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: string;
+  budget: number;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  };
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface CreditsMovie {
+  id: number;
+  cast: {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    order: number;
+  }[];
+
+  crew: {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+    credit_id: string;
+    department: string;
+    job: string;
+  }[];
+}
+
+export interface ImagesMovie {
+  backdrops: {
+    aspect_ratio: number;
+    height: number;
+    iso_639_1: string;
+    file_path: string;
+    vote_average: number;
+    vote_count: number;
+    width: number;
+  }[];
+  id: number;
+  logos: {
+    aspect_ratio: number;
+    height: number;
+    iso_639_1: string;
+    file_path: string;
+    vote_average: number;
+    vote_count: number;
+    width: number;
+  }[];
+  posters: {
+    aspect_ratio: number;
+    height: number;
+    iso_639_1: string;
+    file_path: string;
+    vote_average: number;
+    vote_count: number;
+    width: number;
+  }[];
+}
+
+export interface VideosMovie {
+  id: number;
+  results: {
+    iso_639_1: string;
+    iso_3166_1: string;
+    name: string;
+    key: string;
+    site: string;
+    size: number;
+    type: string;
+    official: boolean;
+    published_at: string;
+    id: string;
+  }[];
+}
+
 export interface Trending {
   adult: boolean;
   backdrop_path: string;
