27dcc79b50adf79a98155ade9865359e5bb0acbd
manikandareas <vitoandareas15@gmail.com>
2023-11-13 12:18:47 +0800

    fix(metadata): fix name opengraph image

--- src/components/meta/Metadata.ts
+++ src/components/meta/Metadata.ts
@@ -18,7 +18,7 @@ export const myMetadata: Metadata = {
       "Madia Film adalah portal film terkini yang memadukan kecanggihan teknologi dengan kecintaan pada seni perfilman. Temukan ribuan judul film dari berbagai genre, mulai dari drama epik hingga petualangan penuh aksi. Nikmati pengalaman menonton yang tak terlupakan dengan fitur pemutaran berkualitas tinggi dan informasi mendalam tentang setiap produksi. Madia Film memberikan akses cepat dan mudah ke dunia film, membawa Anda lebih dekat dengan cerita-cerita paling ikonik dan aktor-aktor terkenal.",
     images: [
       {
-        url: "https://movies-nextjs-coral.vercel.app/og.png",
+        url: "https://movies-nextjs-coral.vercel.app/opengraph-image.png",
       },
     ],
   },
